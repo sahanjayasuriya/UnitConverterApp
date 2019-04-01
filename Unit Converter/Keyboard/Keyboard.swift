58918201bc3f2faa8cc4eaf8da50bba34c35cd13
Sahan Jayasuriya <sahanjayasuriya@Sahans-Mac.local>
2019-04-01 12:44:37 -0700

    Added keyboard

---
+++ Unit Converter/Keyboard/Keyboard.swift
@@ -0,0 +1,21 @@
+//
+//  Keyboard.swift
+//  Unit Converter
+//
+//  Created by Sahan Jayasuriya on 3/30/19.
+//  Copyright © 2019 Sahan Jayasuriya. All rights reserved.
+//
+
+import UIKit
+
+class Keyboard: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
